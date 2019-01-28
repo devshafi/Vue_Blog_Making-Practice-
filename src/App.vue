edc9bf91b119b9e70275630ea4eacb2f84527902
frshafi49 <frshafi49@gmail.com>
2019-01-29 02:03:42 +0600

    added vue-resource http library

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <add-blog></add-blog>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// importing components
+import AddBlog from './components/AddBlog.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+
+  components:{
+    'add-blog': AddBlog
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
   }
+  
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+body{
+  margin:0;
+  font-family: 'Nunito Semibold'
 }
+
 </style>
